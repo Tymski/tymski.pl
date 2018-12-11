d894b8b27b1b4bb831cc078ec0bb6cd2b102dfcf
Tymski <tymon.oziemblewski@gmail.com>
2018-12-11 02:51:56 +0100

    Links added

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 liczbaKropek = 0;
-setInterval(()=>{
+setInterval(() => {
     kropki.innerHTML = ".".repeat(liczbaKropek++ % 4);
-},333)
+}, 333)
+
+function scrollToElement(query) {
+    document.querySelector(query).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollToNextElement(element) {
+    element.parentNode.nextElementSibling.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollToPreviousElement(element) {
+    element.parentNode.previousElementSibling.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
